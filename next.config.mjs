1ebea9123dc3339b20e77cc71b91eaa950fc9737
AnujChhikara <anujchhikara777@gmail.com>
2024-04-11 16:22:52 +0530

    backend end point

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,25 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     images: {
+//         domains: ['res.cloudinary.com', 'images.unsplash.com', 'imgs.search.brave.com']
+//     },
+//     env:{
+//         url: 'http://localhost:8000/api/v1'
+//     }
+// };
+
+// export default nextConfig;
+//for deployment 
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
         domains: ['res.cloudinary.com', 'images.unsplash.com', 'imgs.search.brave.com']
     },
     env:{
-        url: 'http://localhost:8000/api/v1'
+        url: 'https://backend-yt-0y5f.onrender.com/api/v1'
     }
 };
 
 export default nextConfig;
+
